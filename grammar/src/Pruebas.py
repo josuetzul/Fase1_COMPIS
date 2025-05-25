04752ceeec819ebe5eff2ff011aa7b061cc1f9ce
joaquín Choc <joaquinchoc@gmail.com>
2025-05-25 16:34:35 -0600

    Creación de prubas finales

---
+++ grammar/src/Pruebas.py
@@ -0,0 +1,11 @@
+x = 0
+y = 10
+n = 0
+print("Ingrese un numero")
+n = int(input())
+x = n+5
+print(x)
+if x<=y:
+    print("El numero ingresado es menor que 10")
+else:
+    print("El numero ingresado es mayor que 10")
